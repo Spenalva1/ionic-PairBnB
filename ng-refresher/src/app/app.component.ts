a58972c9252c1591e660e1ddaf47307a1aa1708f
spenalva1 <spenalva@prexcard.com>
2021-03-09 17:17:32 -0300

    project cleaned

--- ng-refresher/src/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { stringify } from 'querystring';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent { }
